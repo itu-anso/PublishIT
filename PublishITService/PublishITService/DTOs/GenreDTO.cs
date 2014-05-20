78e810a4f103d92356f284293afdc2d6a0ccfbe3
Rosted <rrosted@gmail.com>
2014-05-20 22:38:40 +0200

    Datamembers på props i MediaDTO

---
+++ PublishITService/PublishITService/DTOs/GenreDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace PublishITService.DTOs
+{
+    public class GenreDTO
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string GenreName { get; set; }
+    }
+}
--- PublishITService/PublishITService/DTOs/MediaDTO.cs
+++ PublishITService/PublishITService/DTOs/MediaDTO.cs
@@ -1,25 +1,39 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace PublishITService.DTOs
 {
     public class MediaDTO
     {
+        [DataMember]
         public int MediaId { get; set; }
+        [DataMember]
         public int UserId { get; set; }
+        [DataMember]
         public int FormatId { get; set; }
+        [DataMember]
         public string Title { get; set; }
+        [DataMember]
         public string Description { get; set; }
+        [DataMember]
         public DateTime? Date { get; set; }
+        [DataMember]
         public string Location { get; set; }
+        [DataMember]
         public double? AvgRating { get; set; }
+        [DataMember]
         public int? NumberOfDownloads { get; set; }
 
         //For video
+        [DataMember]
         public double? Length { get; set; }
+        [DataMember]
         public int? NumberOfRents { get; set; }
+        [DataMember]
         public int? NumberOfTrailerViews { get; set; }
 
         //For document
+        [DataMember]
         public string Status { get; set; }
     }
 }
